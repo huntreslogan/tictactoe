3a22f190e338b95160c66b8ccf87fdf373ee07b7
Jessica Hunt <huntres@me.com>
2015-04-23 13:45:16 -0700

    Fixing heroku issues

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
-var gzippo = require('gzippo');
+// var gzippo = require('gzippo');
 var express = require('express');
-var morgan = require('morgan');
+// var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
 
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(express.static(__dirname + '/public'))
 app.listen(process.env.PORT || 5000);
