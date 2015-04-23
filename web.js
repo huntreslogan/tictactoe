bdf4a86789453c509b83486c98d6f89178362c46
Jessica Hunt <huntres@me.com>
2015-04-23 13:58:27 -0700

    Heroku

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
-// var gzippo = require('gzippo');
+var gzippo = require('gzippo');
 var express = require('express');
-// var morgan = require('morgan');
+var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
 
-// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.use(express.static(__dirname + '/public'))
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
